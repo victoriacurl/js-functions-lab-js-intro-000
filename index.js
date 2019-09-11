25f3ff26c84eff80c1ccdd42b1e39ca01b130b39
Victoria Curl <victoriacurl9@gmail.com>
2019-09-11 21:36:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown(holdiay, days){
+  return `It's ${days} days until ${holiday}!`
+}
